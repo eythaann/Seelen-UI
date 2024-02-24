56a82d119e5d1f2980bcf59bf2a8cfc7fc38ddd1
eythaann <eythan.cvt@gmail.com>
2024-02-24 08:04:26 -0500

    fix not taking config on run ahk at start

--- static/pwsh/manual_run.ps1
+++ static/pwsh/manual_run.ps1
@@ -7,8 +7,15 @@ if ($runningProcesses.Count -eq 0) {
   $task = Get-ScheduledTask -TaskName KomorebiUI -ErrorAction SilentlyContinue
   if ($null -ne $task) { 
     Start-ScheduledTask -TaskName KomorebiUI
-  } else {
+  }
+  else {
+    $jsonPath = Join-Path $Env:USERPROFILE "\.config\komorebi-ui\settings.json"
+    $jsonContent = Get-Content -Raw -Path $jsonPath | ConvertFrom-Json
+
     Start-Process powershell -WindowStyle Hidden -ArgumentList "-NoProfile -Command `"& '$ExeRoute' -c '$Env:USERPROFILE\.config\komorebi-ui\settings.json'`""
-    & (Join-Path $Env:USERPROFILE "\.config\komorebi-ui\komorebic.ahk")
+
+    if ($jsonContent.ahk_enabled -eq $true) {
+      & (Join-Path $Env:USERPROFILE "\.config\komorebi-ui\komorebic.ahk")
+    }
   }
 }
