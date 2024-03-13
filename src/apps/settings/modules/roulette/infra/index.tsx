4159e0d1cb7ddd210ec3bea58aba0137f2b28d67
eythaann <eythan.cvt@gmail.com>
2024-03-13 00:35:45 -0500

    remove unnused files

--- src/apps/settings/modules/roulette/infra/index.tsx
+++
@@ -1,7 +0,0 @@
-import cs from './index.module.css';
-
-export function Roulette() {
-  return <div className={cs.roulette}>
-    <div className={cs.drag} data-tauri-drag-region>➕</div>
-  </div>;
-}
