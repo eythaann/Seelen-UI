985abb42da0d239ca0a29cad118638d388df9141
eythaann <eythan.cvt@gmail.com>
2024-04-28 21:47:25 -0500

    enh(prebuild): replace exec by execSync

--- scripts/prebuild.ts
+++ scripts/prebuild.ts
@@ -1,15 +1,6 @@
-import { exec } from 'child_process';
+import { execSync } from 'child_process';
 
 const command = 'Get-WmiObject Win32_Process | Where-Object { $_.CommandLine -like \'*target\\debug\\*seelen*.ahk\' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force }';
 
-exec(`powershell -ExecutionPolicy Bypass -NoProfile -Command "${command}"`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`PowerShell stderr: ${stderr}`);
-    return;
-  }
-  console.log(`PowerShell stdout: ${stdout}`);
-});
+execSync(`powershell -ExecutionPolicy Bypass -NoProfile -Command "${command}"`);
+execSync(`powershell -ExecutionPolicy Bypass -NoProfile -Command "${command}"`);
