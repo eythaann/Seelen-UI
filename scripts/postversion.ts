4b1465a23f61726c240b75fedc5afc5d9cc4934c
eythaann <eythan.cvt@gmail.com>
2024-05-08 22:04:48 -0500

    Remove unused code on postversion

--- scripts/postversion.ts
+++ scripts/postversion.ts
@@ -1,26 +1,8 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
 
-const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
-
 let changelogContent = fs.readFileSync('changelog.md', 'utf-8');
-
-const regex = /(?<=\[Unreleased\]\s)([\s\S]*?)(?=\s## \[)/g;
-const releaseNotes = changelogContent.match(regex)?.[0].trim() || '';
-
-const variables = {
-  version: packageJson.version,
-  notes: releaseNotes.replaceAll(/\r/g, '').replaceAll(/\n/g, '\\n'),
-  current_date: new Date().toISOString(),
-  signature: fs.readFileSync(`target/release/bundle/nsis/Seelen UI_${packageJson.version}_x64-setup.nsis.zip.sig`, 'utf-8'),
-};
-const jsonString = fs.readFileSync('templates/release.json', 'utf-8');
-const replacedString = jsonString.replace(/{{(.*?)}}/g, (match, p1 ) => {
-  return (variables as any)[p1] || match;
-});
-fs.writeFileSync('release', replacedString);
-
-changelogContent = changelogContent.replace(/## \[Unreleased\]/g, `## \[Unreleased\]\n## [${packageJson.version}]`);
+changelogContent = changelogContent.replace(/## \[Unreleased\]/g, `## \[Unreleased\]\n## [${process.env.npm_package_version}]`);
 fs.writeFileSync('changelog.md', changelogContent);
 
 execSync('git add .');
