45678e6f303ac9acbc215175eebada6020053615
eythaann <eythan.cvt@gmail.com>
2024-06-04 21:48:04 -0500

    add tracing to fullscreen events for toolbar

--- src/background/seelen_bar/hook.rs
+++ src/background/seelen_bar/hook.rs
@@ -5,27 +5,39 @@ use crate::{error_handler::Result, windows_api::WindowsApi, winevent::WinEvent};
 use super::FancyToolbar;
 
 impl FancyToolbar {
-    pub fn process_win_event(&mut self, event: WinEvent, hwnd: HWND) -> Result<()> {
+    pub fn process_win_event(&mut self, event: WinEvent, origin: HWND) -> Result<()> {
         match event {
             WinEvent::ObjectNameChange => {
-                if self.last_focus == Some(hwnd.0) {
-                    self.focus_changed(hwnd)?;
+                if self.last_focus == Some(origin.0) {
+                    self.focus_changed(origin)?;
                 }
             }
             WinEvent::SystemForeground | WinEvent::ObjectFocus => {
-                self.focus_changed(hwnd)?;
+                self.focus_changed(origin)?;
             }
             WinEvent::SyntheticFullscreenStart => {
                 let monitor = WindowsApi::monitor_from_window(self.window.hwnd()?);
-                let event_monitor = WindowsApi::monitor_from_window(hwnd);
+                let event_monitor = WindowsApi::monitor_from_window(origin);
                 if monitor == event_monitor {
+                    log::trace!(
+                        "Fullscreen on {} || {} || {}",
+                        WindowsApi::exe(origin).unwrap_or_default(),
+                        WindowsApi::get_class(origin).unwrap_or_default(),
+                        WindowsApi::get_window_text(origin)
+                    );
                     self.hide()?;
                 }
             }
             WinEvent::SyntheticFullscreenEnd => {
                 let monitor = WindowsApi::monitor_from_window(self.window.hwnd()?);
-                let event_monitor = WindowsApi::monitor_from_window(hwnd);
+                let event_monitor = WindowsApi::monitor_from_window(origin);
                 if monitor == event_monitor {
+                    log::trace!(
+                        "Exit Fullscreen on {} || {} || {}",
+                        WindowsApi::exe(origin).unwrap_or_default(),
+                        WindowsApi::get_class(origin).unwrap_or_default(),
+                        WindowsApi::get_window_text(origin)
+                    );
                     self.show()?;
                 }
             }
