28b40e82fc59766aba0e1804b5ac3bd28468d361
eythaann <eythan.cvt@gmail.com>
2024-06-25 13:33:02 -0500

    Update workflow

--- scripts/SubmitToStore.ps1
+++ scripts/SubmitToStore.ps1
@@ -1,12 +1,11 @@
-<# param([string]$appxPathParam)
-
-if (-not (Test-Path -Path $PSBoundParameters["appxPathParam"])) {
-  Write-Error "The appxupload file does not exist. Double check that you have passed the file path as a parameter and the file exists." -ErrorAction Stop
-} #>
-
 Set-ExecutionPolicy RemoteSigned -Force
 
 # Prepare materials from previous steps and runner environment variables
+$jsonContent = Get-Content -Path ".\package.json" -Raw
+$jsonObject = $jsonContent | ConvertFrom-Json
+$version = $jsonObject.version
+$appxUploadFilePath = ".\target\release\bundle\msix\Seelen.SeelenUI_${version}_x64__p6yyn03m1894e.msix"
+
 # $appxUploadFilePath = $PSBoundParameters["appxPathParam"]
 $username = $env:PartnerCenterClientId
 $password = ConvertTo-SecureString $env:PartnerCenterClientSecret -AsPlainText -Force
@@ -14,10 +13,6 @@ $appStoreId = $env:PartnerCenterStoreId
 $tenantId = $env:PartnerCenterTenantId
 
 $scriptPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-
-$url = "https://github.com/eythaann/Seelen-UI/releases/latest/download/Seelen.UI_Temporal_Unsigned.msix"
-$appxUploadFilePath = Join-Path -Path $scriptPath -ChildPath 'Seelen.UI_Temporal_Unsigned.msix'
-Invoke-WebRequest -Uri $url -OutFile $appxUploadFilePath
 
 # ********* Create temporary directory for submission artifacts *********
 $sbTempFolderPath = New-Item -Type Directory -Force -Path (Join-Path -Path $scriptPath -ChildPath 'SBTemp')
@@ -38,4 +33,4 @@ New-SubmissionPackage -ConfigPath $configFilePath -AppxPath $appxUploadFilePath 
 $submissionDataPath = Join-Path -Path $sbTempFolderPath -ChildPath 'submission.json'
 $submissionPackagePath = Join-Path -Path $sbTempFolderPath -ChildPath 'submission.zip'
 
-Update-ApplicationSubmission -Verbose -Force -ReplacePackages -AppId $appStoreId -SubmissionDataPath $submissionDataPath -PackagePath $submissionPackagePath -NoStatus
+Update-ApplicationSubmission -Verbose -Force -ReplacePackages -AutoCommit -AppId $appStoreId -SubmissionDataPath $submissionDataPath -PackagePath $submissionPackagePath -NoStatus
