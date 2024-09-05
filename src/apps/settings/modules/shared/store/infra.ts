390fd865bab454930ffa9d5883db0c49de2038d1
eythaann <eythan.cvt@gmail.com>
2024-09-04 22:20:05 -0500

    fix(themes): no updating on settings change

--- src/apps/settings/modules/shared/store/infra.ts
+++ src/apps/settings/modules/shared/store/infra.ts
@@ -76,6 +76,7 @@ export const LoadSettingsToStore = async (customPath?: string) => {
     .withLayouts()
     .withPlaceholders()
     .withUserApps()
+    .withThemes()
     .withWallpaper()
     .load(customPath);
 
