143e0228efda6b85788fde2a85f49003a4b55b9c
eythaann <eythan.cvt@gmail.com>
2024-10-03 07:30:49 -0500

    fix(wall): app crashing when trying set custom wallpapers

--- src/background/seelen_wall/mod.rs
+++ src/background/seelen_wall/mod.rs
@@ -8,7 +8,10 @@ use windows::Win32::{
 };
 
 use crate::{
-    error_handler::Result, log_error, pcstr, seelen::get_app_handle, windows_api::WindowsApi,
+    error_handler::Result,
+    log_error, pcstr,
+    seelen::get_app_handle,
+    windows_api::{WindowEnumerator, WindowsApi},
 };
 
 pub struct SeelenWall {
@@ -62,7 +65,7 @@ impl SeelenWall {
         let mut rect = WindowsApi::virtual_screen_rect()?;
         let main_hwnd = HWND(self.window.hwnd()?.0);
 
-        if Self::try_set_under_progman(main_hwnd).is_ok() {
+        if Self::try_set_inside_workerw(main_hwnd).is_ok() {
             // rect relative to the parent
             rect = RECT {
                 top: 0,
@@ -75,44 +78,75 @@ impl SeelenWall {
         // pre set position for resize in case of multiples dpi
         WindowsApi::move_window(main_hwnd, &rect)?;
         WindowsApi::set_position(main_hwnd, None, &rect, SWP_NOACTIVATE)?;
-        Self::refresh_desktop()?;
+        log_error!(Self::refresh_desktop().map_err(|e| format!("Failed to refresh desktop: {}", e)));
         Ok(())
     }
 
-    fn try_set_under_progman(hwnd: HWND) -> Result<()> {
+    fn try_set_inside_workerw(hwnd: HWND) -> Result<()> {
         let progman = unsafe { FindWindowA(pcstr!("Progman"), None)? };
 
         // Send 0x052C to Progman. This message directs Progman to spawn a WorkerW
         // behind the desktop icons. If it is already there, nothing happens.
         unsafe { PostMessageW(progman, 0x052C, WPARAM(0xD), LPARAM(0x1))? };
 
-        let mut worker =
-            unsafe { FindWindowExA(progman, HWND::default(), pcstr!("WorkerW"), None) };
-        let mut attempts = 0;
-        while worker.is_err() && attempts < 10 {
-            attempts += 1;
-            std::thread::sleep(std::time::Duration::from_millis(100));
-            worker = unsafe { FindWindowExA(progman, HWND::default(), pcstr!("WorkerW"), None) };
+        // CASE 1:
+        // 0x00010190 "" WorkerW
+        //   ...
+        //   0x000100EE "" SHELLDLL_DefView
+        //     0x000100F0 "FolderView" SysListView32
+        // 0x00100B8A "" WorkerW       <-- This is the WorkerW instance we are after!
+        // 0x000100EC "Program Manager" Progman
+        let mut worker = None;
+
+        WindowEnumerator::new().for_each(|current| unsafe {
+            // check if current contains SHELLDLL_DefView
+            if FindWindowExA(current, None, pcstr!("SHELLDLL_DefView"), None).is_ok() {
+                // find next worker after the current one
+                if let Ok(_worker) = FindWindowExA(None, current, pcstr!("WorkerW"), None) {
+                    worker = Some(_worker);
+                }
+            }
+        })?;
+
+        // CASE 2:
+        // Some Windows 11 builds have a different Progman window layout.
+        // If the above code failed to find WorkerW, we should try this.
+        // 0x000100EC "Program Manager" Progman
+        //   0x000100EE "" SHELLDLL_DefView
+        //     0x000100F0 "FolderView" SysListView32
+        //   0x00100B8A "" WorkerW       <-- This is the WorkerW instance we are after!
+        if worker.is_none() {
+            let mut attempts = 0;
+            worker =
+                unsafe { FindWindowExA(progman, HWND::default(), pcstr!("WorkerW"), None).ok() };
+            while worker.is_none() && attempts < 10 {
+                attempts += 1;
+                std::thread::sleep(std::time::Duration::from_millis(100));
+                worker = unsafe {
+                    FindWindowExA(progman, HWND::default(), pcstr!("WorkerW"), None).ok()
+                };
+            }
         }
 
         match worker {
-            Ok(w) => {
-                unsafe {
-                    SetParent(hwnd, w)?;
-                }
+            Some(worker) => {
+                unsafe { SetParent(hwnd, worker)? };
                 Ok(())
             }
-            Err(_) => Err("Failed to find/create progman worker window".into()),
+            None => Err("Failed to find/create progman worker window".into()),
         }
     }
 
+    /// this is only needed on the case 2 of try_set_inside_workerw
     fn refresh_desktop() -> Result<()> {
         unsafe {
             let progman = FindWindowA(pcstr!("Progman"), None)?;
-            let shell_view =
-                FindWindowExA(progman, HWND::default(), pcstr!("SHELLDLL_DefView"), None)?;
-            InvalidateRect(shell_view, None, true).ok()?;
-            UpdateWindow(shell_view).ok()?;
+            if let Ok(shell_view) =
+                FindWindowExA(progman, HWND::default(), pcstr!("SHELLDLL_DefView"), None)
+            {
+                InvalidateRect(shell_view, None, true).ok()?;
+                UpdateWindow(shell_view).ok()?;
+            }
         }
         Ok(())
     }
