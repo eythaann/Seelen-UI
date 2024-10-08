0724e5028259916229ecaabe348d271e2a2ae649
eythaann <eythan.cvt@gmail.com>
2024-10-08 05:29:40 -0500

    fix(#119): modules moving of place after startup

--- src/background/seelen_weg/hook.rs
+++ src/background/seelen_weg/hook.rs
@@ -52,11 +52,15 @@ impl SeelenWeg {
     }
 
     pub fn process_individual_win_event(&mut self, event: WinEvent, origin: HWND) -> Result<()> {
+        let window = Window::from(origin);
         match event {
             WinEvent::SystemForeground | WinEvent::ObjectFocus => {
                 self.handle_overlaped_status(origin)?;
             }
             WinEvent::ObjectLocationChange => {
+                if window.title() == Self::TITLE {
+                    self.set_position(window.monitor().raw())?;
+                }
                 if origin == WindowsApi::get_foreground_window() {
                     self.handle_overlaped_status(origin)?;
                 }
