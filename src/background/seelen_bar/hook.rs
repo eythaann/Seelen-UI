3a1e43860023349486383df7d67a757613336a49
eythaann <eythan.cvt@gmail.com>
2024-10-08 14:15:06 -0500

    fix(monitors): widgets only be shown on one monitor

--- src/background/seelen_bar/hook.rs
+++ src/background/seelen_bar/hook.rs
@@ -22,7 +22,7 @@ impl FancyToolbar {
                 self.handle_overlaped_status(origin)?;
             }
             WinEvent::ObjectLocationChange => {
-                if window.title() == Self::TITLE {
+                if window.hwnd() == self.window.hwnd()? {
                     self.set_position(window.monitor().raw())?;
                 }
                 if origin == WindowsApi::get_foreground_window() {
