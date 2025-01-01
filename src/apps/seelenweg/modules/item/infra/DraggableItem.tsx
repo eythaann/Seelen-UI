eead3960031f69de736bbbb8bd19d0a91e6d0906
lurius <lurius.tm@gmail.com>
2025-01-01 22:16:04 +0100

    fix(weg): drag state use both solutions

--- src/apps/seelenweg/modules/item/infra/DraggableItem.tsx
+++ src/apps/seelenweg/modules/item/infra/DraggableItem.tsx
@@ -1,5 +1,5 @@
 import { Reorder } from 'framer-motion';
-import { PropsWithChildren, useState } from 'react';
+import { PropsWithChildren, useRef, useState } from 'react';
 
 import { SwItem } from '../../shared/store/domain';
 
@@ -13,18 +13,22 @@ interface Props extends PropsWithChildren {
 
 export function DraggableItem({ children, item, className, drag }: Props) {
   const [ isDragging, setDragging ] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
 
   return (
     <Reorder.Item
       as="div"
+      ref={ref}
       value={item}
       drag={drag}
       className={cx('weg-item-drag-container', className, { 'dragging': isDragging })}
       onDragStart={() => {
+        ref.current?.classList.add('dragging');
         setDragging(true);
       }}
       onDragEnd={() => {
         setTimeout(() => {
+          ref.current?.classList.remove('dragging');
           setDragging(false);
         }, 150);
       }}
