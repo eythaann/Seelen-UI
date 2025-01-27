e2903b5809f5f975d0e94e39c1694335ee2e6a54
eythaann <eythan.cvt@gmail.com>
2025-01-27 15:54:02 -0500

    fix(tb): infinity migration

--- src/background/restoration_and_migrations/mod.rs
+++ src/background/restoration_and_migrations/mod.rs
@@ -20,10 +20,16 @@ impl RestorationAndMigration {
         Ok(())
     }
 
+    // migration of user settings files below v2.1.0, will be removed in v3.0
     pub fn migrate_old_toolbar_items() -> Result<()> {
-        let old = SEELEN_COMMON.user_placeholders_path().join("custom.yml");
+        let old_folder = SEELEN_COMMON.user_placeholders_path();
+        let old = old_folder.join("custom.yml");
         if old.exists() {
             std::fs::copy(old, SEELEN_COMMON.toolbar_items_path())?;
+            std::fs::rename(
+                old_folder,
+                old_folder.with_file_name("deprecated_placeholders"),
+            )?;
         }
         Ok(())
     }
