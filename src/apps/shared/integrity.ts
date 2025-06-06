40ce2e7d1de731369fd55735151e1a2d0fe424b9
eythaann <eythan.cvt@gmail.com>
2025-06-06 17:14:09 -0500

    enh(integrity): remove unneeded code

--- src/apps/shared/integrity.ts
+++ src/apps/shared/integrity.ts
@@ -1,6 +1,10 @@
+// before tauri v2.5 this script was done as a workaround to https://github.com/tauri-apps/tauri/issues/12348
+// but was fixed on https://github.com/tauri-apps/wry/pull/1531 so now this script is used to initialize
+// the console logger to capture any error on the main script
+
 import { wrapConsoleV2 } from './ConsoleWrapper';
 
-async function WaitForTauriInternals(): Promise<void> {
+/* async function WaitForTauriInternals(): Promise<void> {
   await new Promise<void>((resolve) => {
     const checkInterval = setInterval(() => {
       if ('__TAURI_INTERNALS__' in window) {
@@ -11,13 +15,13 @@ async function WaitForTauriInternals(): Promise<void> {
   });
 }
 
-await WaitForTauriInternals();
+await WaitForTauriInternals(); */
 wrapConsoleV2();
 
-const script = document.createElement('script');
+/* const script = document.createElement('script');
 script.src = './index.js';
 script.type = 'module';
 script.defer = true;
-document.head.appendChild(script);
+document.head.appendChild(script); */
 
 export {};
