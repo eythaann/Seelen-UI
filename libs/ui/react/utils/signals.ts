b31d9094b3f2ea0843bd90b7726de3d76f5dcefb
eythaann <eythan.cvt@gmail.com>
2026-04-27 21:52:10 -0500

    fix(dock): auto-hide stucked state

--- libs/ui/react/utils/signals.ts
+++ libs/ui/react/utils/signals.ts
@@ -1,15 +1,8 @@
-import { UIColors, Widget } from "@seelen-ui/lib";
+import { SeelenEvent, subscribe, Widget } from "@seelen-ui/lib";
 import { lazySignal } from "./LazySignal";
 
-const window = Widget.self.window;
-
-export const $is_this_webview_focused = lazySignal(() => window.isFocused());
-await window.onFocusChanged(async () => {
-  // the payload value is not used, cuz on startup it gives wrong value.
-  $is_this_webview_focused.value = await window.isFocused();
+export const $is_this_webview_focused = lazySignal(() => Widget.self.window.isFocused());
+await subscribe(SeelenEvent.GlobalFocusChanged, ({ payload: { hwnd, ownerHwnd } }) => {
+  $is_this_webview_focused.value = Widget.self.windowId === hwnd || Widget.self.windowId === ownerHwnd;
 });
 await $is_this_webview_focused.init();
-
-export const $system_colors = lazySignal(async () => (await UIColors.getAsync()).inner);
-await UIColors.onChange((colors) => ($system_colors.value = colors.inner));
-await $system_colors.init();
