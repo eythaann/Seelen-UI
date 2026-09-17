b140828fddea97a0d1a4d5002ad0e0517c381deb
Benin <benindev06@gmail.com>
2026-09-17 19:00:20 +0530

    feat(ui): redesign power menu layout and interactions

--- src/ui/svelte/power-menu/app.svelte
+++ src/ui/svelte/power-menu/app.svelte
@@ -1,29 +1,40 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
   import { options } from "./options";
-  import { state } from "./state.svelte";
+  import { state as globalState } from "./state.svelte";
   import { convertFileSrc } from "@tauri-apps/api/core";
   import Icon from "libs/ui/svelte/components/Icon/Icon.svelte";
   import { Widget } from "@seelen-ui/lib";
   import { t } from "./i18n";
   import { MissingIcon } from "libs/ui/svelte/components/Icon";
 
+  let isVisible = $state(false);
+
   onMount(() => {
     Widget.getCurrent().ready();
+    isVisible = true;
+
+    window.addEventListener("focus", () => {
+      isVisible = true;
+    });
   });
 
   function onCancel() {
-    Widget.self.hide();
+    isVisible = false;
+    setTimeout(() => {
+      Widget.self.hide();
+    }, 200);
   }
 
   const menu = $derived.by(() => {
-    if (!state.primaryMonitor) {
+    if (!globalState.primaryMonitor) {
       return null;
     }
 
     const {
       primaryMonitor: { rect, scaleFactor },
-    } = state;
+    } = globalState;
 
     return {
       x: rect.left,
@@ -36,57 +47,69 @@
   });
 </script>
 
-<div
-  class="power-menu-overlay"
-  role="menu"
-  tabindex="-1"
-  onclick={onCancel}
-  onkeydown={(e) => {
-    if (e.key === "Escape") {
-      onCancel();
-    }
-  }}
->
-  {#if menu}
-    <div
-      class="power-menu"
-      style:position="fixed"
-      style:left={menu.x + "px"}
-      style:top={menu.y + "px"}
-      style:width={menu.width + "px"}
-      style:height={menu.height + "px"}
-      style:transform={`scale(${menu.scale})`}
-      style:transform-origin="left top"
-    >
-      <div class="power-menu-user">
-        {#if state.user.profilePicturePath}
-          <img
-            class="power-menu-user-profile"
-            src={convertFileSrc(state.user.profilePicturePath)}
-            alt=""
-          />
-        {:else}
-          <MissingIcon class="power-menu-user-profile" />
-        {/if}
-        <div class="power-menu-user-email">
-          {state.user.email}
+<svelte:window onkeydown={(e) => {
+  if (e.key === "Escape") {
+    onCancel();
+  }
+}} />
+
+{#if isVisible}
+  <div
+    class="power-menu-overlay"
+    role="menu"
+    tabindex="-1"
+    onclick={onCancel}
+    transition:fade={{ duration: 200 }}
+  >
+    {#if menu}
+      <div
+        class="power-menu"
+        style:position="fixed"
+        style:left={menu.x + "px"}
+        style:top={menu.y + "px"}
+        style:width={menu.width + "px"}
+        style:height={menu.height + "px"}
+        style:transform={`scale(${menu.scale})`}
+        style:transform-origin="left top"
+      >
+        <div class="power-popup" onclick={(e) => e.stopPropagation()}>
+          <div class="power-menu-header">
+            <div class="power-menu-user">
+              {#if globalState.user.profilePicturePath}
+                <img
+                  class="power-menu-user-profile"
+                  src={convertFileSrc(globalState.user.profilePicturePath)}
+                  alt=""
+                />
+              {:else}
+                <MissingIcon class="power-menu-user-profile" />
+              {/if}
+              <div class="power-menu-user-info">
+                <div class="power-menu-bye-bye">{$t("goodbye", { 0: globalState.user.name })}</div>
+                <div class="power-menu-user-email">{globalState.user.email}</div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="power-menu-separator"></div>
+
+          <ul class="power-menu-list">
+            {#each options as option}
+              <li>
+                <button onclick={() => { option.onClick(); onCancel(); }} class="power-menu-item">
+                  <div class="power-menu-item-icon-wrapper">
+                    <Icon iconName={option.icon as any} />
+                  </div>
+                  <span class="power-menu-item-label">{$t(option.key)}</span>
+                </button>
+              </li>
+            {/each}
+          </ul>
         </div>
       </div>
-      <div class="power-menu-bye-bye">{$t("goodbye", { 0: state.user.name })}</div>
-      <ul class="power-menu-list">
-        {#each options as option}
-          <li>
-            <button onclick={option.onClick} class="power-menu-item">
-              <Icon iconName={option.icon as any} />
-              <span class="power-menu-item-label">{$t(option.key)}</span>
-            </button>
-          </li>
-        {/each}
-      </ul>
-      <!-- <div class="power-menu-uptime">{$t("uptime")}: 2 hours 30 minutes</div> -->
-    </div>
-  {/if}
-</div>
+    {/if}
+  </div>
+{/if}
 
 <style>
   :global(body) {
